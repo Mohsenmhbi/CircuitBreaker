21cabef2f4c66542d73bdf6dee19e22822982455
mohsen.mhbi <mohsen.mhbi@gmail.com>
2021-03-22 16:17:26 +0430

    Init

---
+++ Model/Message.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Model
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+
+}
---
+++ Model/MessageClient.cs
@@ -0,0 +1,9 @@
+﻿namespace Model
+{
+    public class MessageClient: Message
+    {
+        public int Id { get; set; }
+        public string ClientText { get; set; }
+    }
+
+}

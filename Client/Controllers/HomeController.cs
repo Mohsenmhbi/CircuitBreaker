5b01ff86ffae83dd0a7af0f642dd62dd3462e7d9
mohsen.mhbi <mohsen.mhbi@gmail.com>
2021-03-22 17:03:56 +0430

    Add File

--- Client/Controllers/HomeController.cs
+++ Client/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Model;
+using Newtonsoft.Json;
+using Polly;
+using Polly.Retry;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace CircuitBreaker.Client.Controllers
@@ -11,6 +16,23 @@ namespace CircuitBreaker.Client.Controllers
     [Route("[controller]")]
     public class HomeController : ControllerBase
     {
-   
+        private readonly AsyncRetryPolicy<HttpResponseMessage> _retryPolicy;
+        private readonly string _baseUrl = "";
+        public HomeController()
+        {
+            _retryPolicy = Policy.HandleResult<HttpResponseMessage>(result =>
+              result.IsSuccessStatusCode).RetryAsync(5, (a, b) => {
+                  string c = "Retry";
+              });
+        }
+
+        public async Task< IActionResult> Index()
+        {
+            HttpClient client = new HttpClient();
+            var result =await _retryPolicy.ExecuteAsync(()=> client.GetAsync(_baseUrl)) ;
+            var str = await result.Content.ReadAsStringAsync();
+            var obj = JsonConvert.DeserializeObject<MessageClient>(str);
+            return Ok(obj);
+        }
     }
 }
